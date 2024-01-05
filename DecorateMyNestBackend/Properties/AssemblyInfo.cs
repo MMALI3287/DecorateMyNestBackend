64062aac01f677ad55b02a7ce9fa4ac290a8838d
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2024-01-05 09:09:00 +0600

    Application Layer Renamed

--- DecorateMyNestBackend/Properties/AssemblyInfo.cs
+++ DecorateMyNestBackend/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DecorateMyNest")]
+[assembly: AssemblyTitle("DecorateMyNestBackend")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DecorateMyNest")]
+[assembly: AssemblyProduct("DecorateMyNestBackend")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
